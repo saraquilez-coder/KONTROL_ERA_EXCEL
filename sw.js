d9ff8efbf2ff3b770a6f460203444b861b961e65
saraquilez-coder <saraquilez@gmail.com>
2026-04-21 17:20:21 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kontrol-era-v25';
+const CACHE_NAME = 'kontrol-era-v26';
 const assets = ['./', './index.html', './style.css', './script.js', './manifest.json'];
 
 self.addEventListener('install', e => {
